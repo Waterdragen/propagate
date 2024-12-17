96e0d5e0f4bd9ce7e4213ee676bc6980f8312b68
Waterdragen <waterdragenarchive@gmail.com>
2024-12-18 01:58:39 +0800

    Add tests

---
+++ tests/test_good.rs
@@ -0,0 +1,131 @@
+use propagate::{Propagate, good};
+mod util;
+use core::ops::ControlFlow::{self, Break, Continue};
+
+#[derive(Debug, PartialEq, Propagate)]
+#[allow(dead_code)]
+enum MyEnum {
+    #[good]
+    Zero,
+    #[good]
+    One(i32),
+    #[good]
+    OverloadOne(i32),
+    #[good]
+    Two(i32, i32),
+    Three(i32, i32, i32),
+    Named {
+        id: i32,
+    },
+}
+
+#[test]
+fn good_value_or_return_self() {
+    let opt: Option<i32> = Some(1);
+    assert_unwrap_eq!(good!(opt), 1);
+    let opt: Option<i32> = None;
+    assert_short_circuit_eq!(good!(opt), None);
+
+    let res: Result<i32, &str> = Ok(2);
+    assert_unwrap_eq!(good!(res), 2);
+    let res: Result<i32, &str> = Err("error");
+    assert_short_circuit_eq!(good!(res), Err("error"));
+
+    let control: ControlFlow<&str, i32> = Continue(3);
+    assert_unwrap_eq!(good!(control), 3);
+    let control: ControlFlow<&str, i32> = Break("break");
+    assert_short_circuit_eq!(good!(control), Break("break"));
+
+    let my_enum = MyEnum::Zero;
+    assert_unwrap_eq!(good!(my_enum), ());
+    let my_enum = MyEnum::One(1);
+    assert_unwrap_eq!(good!(my_enum), 1);
+    let my_enum = MyEnum::OverloadOne(1);
+    assert_unwrap_eq!(good!(my_enum), 1);
+    let my_enum = MyEnum::Two(2, 4);
+    assert_unwrap_eq!(good!(my_enum), (2, 4));
+    let my_enum = MyEnum::Three(3, 6, 9);
+    assert_short_circuit_eq!(good!(my_enum), i32, MyEnum::Three(3, 6, 9));
+    let my_enum = MyEnum::Zero;
+    assert_short_circuit_eq!(good!(my_enum), i32, MyEnum::Zero);
+}
+
+#[test]
+fn good_value_or_return_default_value() {
+    let opt: Option<i32> = Some(1);
+    assert_unwrap_eq!(good!(opt;), 1);
+    let opt: Option<i32> = None;
+    assert_short_circuit_eq!(good!(opt;), ());
+
+    let res: Result<i32, &str> = Ok(2);
+    assert_unwrap_eq!(good!(res;), 2);
+    let res: Result<i32, &str> = Err("error");
+    assert_short_circuit_eq!(good!(res; return), ());
+
+    let control: ControlFlow<&str, i32> = Continue(3);
+    assert_unwrap_eq!(good!(control; "default value"), 3);
+    let control: ControlFlow<&str, i32> = Break("break");
+    assert_short_circuit_eq!(good!(control; "default value"), "default value");
+}
+
+#[test]
+fn good_value_or_use_default_value() {
+    let opt: Option<i32> = Some(1);
+    assert_unwrap_eq!(good!(opt; else 10), 1);
+    let opt: Option<i32> = None;
+    assert_unwrap_eq!(good!(opt; else 10), 10);
+
+    let opt: Option<i32> = Some(1);
+    assert_unwrap_eq!(good!(opt; default), 1);
+    let opt: Option<i32> = None;
+    assert_unwrap_eq!(good!(opt; default), 0);
+}
+
+#[test]
+fn good_value_or_continue() {
+    let res: Result<i32, &str> = Ok(2);
+    assert_unwrap_eq!(good!(res; continue), 2);
+    let res: Result<i32, &str> = Err("error");
+    assert_continue!(good!(res; continue));
+}
+
+#[test]
+fn good_value_or_break() {
+    let res: Result<i32, &str> = Ok(1);
+    assert_unwrap_eq!(good!(res; break), 1);
+    let res: Result<i32, &str> = Err("error");
+    assert_break_eq!(good!(res; break), ());
+
+    let opt: Option<i32> = Some(2);
+    assert_unwrap_eq!(good!(opt; break "break"), 2);
+    let opt: Option<i32> = None;
+    assert_break_eq!(good!(opt; break "break"), "break");
+}
+
+#[test]
+fn good_value_or_apply_closure_not_two_states() {
+    let my_enum = MyEnum::Zero;
+    assert_unwrap_eq!(good!(my_enum => full break |enum_| matches!(enum_, MyEnum::One{..})), ());
+    let my_enum = MyEnum::One(1);
+    assert_break_eq!(good!(my_enum => full break |enum_| matches!(enum_, MyEnum::One{..})), (i32, i32), true);
+
+    let mut flag = false;
+    let my_enum = MyEnum::Named {id: 0};
+    assert_short_circuit_eq!(good!(my_enum => full do |_| {flag = true;}; 4), i32, 4);
+    assert!(flag);
+}
+
+#[test]
+fn good_value_or_apply_closure_two_states() {
+    let res: Result<i32, &str> = Err("error");
+    assert_short_circuit_eq!(good!(res => |err| err), "error");
+
+    let res: Result<i32, &str> = Err("12");
+    assert_unwrap_eq!(good!(res => else |err: &str| err.parse().unwrap()), 12);
+
+    let res: Result<i32, &str> = Err("error");
+    assert_short_circuit_eq!(good!(res => |err: &str| err.chars().next().unwrap()), 'e');
+
+    let res: Result<i32, &str> = Err("error");
+    assert_break_eq!(good!(res => break |err: &str| err[..3].to_owned()), "err".to_owned());
+}
---
+++ tests/util/mod.rs
@@ -0,0 +1,93 @@
+#[macro_export]
+macro_rules! _must_unwrap {
+    ($mac_call:expr) => {{
+        let mut value: Option<_> = None;
+        let _ = (|| {
+            let mut counter = 0;
+            let _ = loop {
+                counter += 1;
+                if counter > 1 {
+                    unreachable!("Macro reached continue statement");
+                }
+                value = Some($mac_call);
+                #[allow(invalid_value)]
+                return unsafe { ::core::mem::MaybeUninit::<_>::uninit().assume_init()}
+            };
+            #[allow(unreachable_code)]
+            { unreachable!("Macro reached break statement"); }
+        })();
+        value
+    }};
+}
+
+#[macro_export]
+macro_rules! _must_break {
+    ($mac_call:expr, $ty:ty) => {{
+        let mut counter = 0;
+        let break_value = loop {
+            counter += 1;
+            if counter > 1 {
+                unreachable!("Macro reached continue statement");
+            }
+            let _: $ty = $mac_call;
+            #[allow(unreachable_code)]
+            return unreachable!("Macro did not break or continue");
+        };
+        break_value
+    }};
+    ($mac_call:expr) => { _must_break!($mac_call, _) };
+}
+
+/// Asserts the macro should give the inner value,
+/// and value should equal right hand side
+#[macro_export]
+macro_rules! assert_unwrap_eq {
+    ($mac_call:expr, $good_expr:expr) => {
+        assert_eq!($crate::_must_unwrap!($mac_call), Some($good_expr), "Macro short circuited")
+    };
+}
+
+/// Asserts the macro should short circuit,
+/// and the returned value should equal right hand side
+#[macro_export]
+macro_rules! assert_short_circuit_eq {
+    ($mac_call:expr, $return_expr:expr) => {
+        assert_short_circuit_eq!($mac_call, _, $return_expr)
+    };
+    ($mac_call:expr, $ty:ty, $bad_expr:expr) => {{
+        assert_eq!(
+            (|| {
+                let _: $ty = $mac_call;
+                unreachable!("Macro did not short circuit");
+            })(), 
+            $bad_expr
+        );
+    }};
+}
+
+/// Asserts the macro should continue
+#[macro_export]
+macro_rules! assert_continue {
+    ($mac_call:expr, $ty:ty) => {{
+        use core::ops::ControlFlow::{self, Continue, Break};
+        let mut control: ControlFlow<()> = Break(());
+        for i in (0..=1) {
+            if i == 1 {
+                control = Continue(());
+                break;
+            }
+            let _: $ty = $mac_call;
+            unreachable!("Macro did not continue");
+        }
+        assert_eq!(control, Continue(()));
+    }};
+    ($mac_call:expr) => { assert_continue!($mac_call, _) };
+}
+
+#[macro_export]
+macro_rules! assert_break_eq {
+    ($mac_call:expr, $ty:ty, $break_expr:expr) => {
+        assert_eq!(_must_break!($mac_call, $ty), $break_expr);
+    };
+    ($mac_call:expr, $break_expr:expr) => { assert_break_eq!($mac_call, _, $break_expr) }
+}
